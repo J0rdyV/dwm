e9cbb39a99a10672e9ed8c72b9e1999ffbb8ba5e
JordyV <28193980+J0rdyV@users.noreply.github.com>
2021-03-10 20:39:20 +0100

    Moved screenshot binding from u to s

--- config.h
+++ config.h
@@ -105,7 +105,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ShiftMask,             XK_u,      spawn,          {.v = screenshot } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = screenshot } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slock } },
 };
 
