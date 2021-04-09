e74d598188de8fba9a75b766e55b35c0848cbc3a
JordyV <28193980+J0rdyV@users.noreply.github.com>
2021-04-09 18:42:56 +0200

    Small changes to keybindings / floating dmenu

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int gappx              = 0;        /* gap pixel between windows */
 static const char *fonts[]          = { "Cascadia Code:style=Regular:size=11:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Cascadia Code:style=Regular:size=11:antialias=true:autohint=true";
+static const char dmenufont[]       = "Cascadia Code:style=Regular:size=14:antialias=true:autohint=true";
 static const char col_gray1[]       = "#1d2021"; /* bg-dark */
 static const char col_gray2[]       = "#504945"; /* border1 */
 static const char col_gray3[]       = "#7c6f64"; /* fg-dark */
@@ -60,11 +60,12 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "15", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *cooltermcmd[]  = { "cool-retro-term", NULL };
 static const char *slock[]  = { "slock", NULL };
 static const char *screenshot[]  = { "/usr/bin/screen", NULL };
+static const char *screenshotsave[]  = { "/usr/bin/screen-save", NULL };
 static const char *clipmenucmd[] = { "clipmenu", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 
 static Key keys[] = {
@@ -76,8 +77,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	//{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	//{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_y,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
@@ -106,6 +107,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY,                       XK_s,      spawn,          {.v = screenshot } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = screenshotsave } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slock } },
 };
 
