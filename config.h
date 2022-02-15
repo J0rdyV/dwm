c75de42daa55b1a3f7617ec9c6469bb396a802d7
JordyV <28193980+J0rdyV@users.noreply.github.com>
2022-02-15 11:55:53 +0100

    Updates

--- config.h
+++ config.h
@@ -62,18 +62,19 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "15", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *cooltermcmd[]  = { "cool-retro-term", NULL };
 static const char *slock[]  = { "slock", NULL };
 static const char *screenshot[]  = { "/usr/bin/screen", NULL };
+static const char *dtime[]  = { "/usr/bin/dtime", NULL };
 static const char *screenshotsave[]  = { "/usr/bin/screen-save", NULL };
 static const char *clipmenucmd[] = { "clipmenu", "-c", "-l", "15", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *emojimenucmd[]  = { "/usr/bin/emojimenu", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = emojimenucmd } },
 	{ MODKEY,                       XK_v,      spawn,          {.v = clipmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_minus,  spawn,          {.v = cooltermcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = +1 } },
@@ -107,6 +108,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY,                       XK_s,      spawn,          {.v = screenshot } },
+	{ MODKEY,                       XK_t,      spawn,          {.v = dtime } },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = screenshotsave } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slock } },
 };
