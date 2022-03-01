34c9171b97fc9ac4cb4cb758b9c217b9bafa6094
J0rdyV <28193980+J0rdyV@users.noreply.github.com>
2022-03-01 11:40:25 +0100

    Updated fonts

--- config.h
+++ config.h
@@ -6,8 +6,10 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int gappx              = 0;        /* gap pixel between windows */
-static const char *fonts[]          = { "Cascadia Code:style=Regular:size=11:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Cascadia Code:style=Regular:size=14:antialias=true:autohint=true";
+static const char *fonts[]          = {
+	"Cascadia Code PL:style=Regular:size=11:antialias=true:autohint=true",
+	"Noto Emoji:size=11"
+};
 static const char col_gray1[]       = "#1d2021"; /* bg-dark */
 static const char col_gray2[]       = "#504945"; /* border1 */
 static const char col_gray3[]       = "#7c6f64"; /* fg-dark */
@@ -60,13 +62,13 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "15", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "15", "-m", dmenumon, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *slock[]  = { "slock", NULL };
 static const char *screenshot[]  = { "/usr/bin/screen", NULL };
 static const char *dtime[]  = { "/usr/bin/dtime", NULL };
 static const char *screenshotsave[]  = { "/usr/bin/screen-save", NULL };
-static const char *clipmenucmd[] = { "clipmenu", "-c", "-l", "15", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *clipmenucmd[] = { "clipmenu", "-c", "-l", "15", "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *emojimenucmd[]  = { "/usr/bin/emojimenu", NULL };
 
 static Key keys[] = {
