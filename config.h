d14c8abfb41fe1f57c7e81651767f2017d9f140b
J0rdyV <28193980+J0rdyV@users.noreply.github.com>
2022-04-20 09:56:49 +0200

    Added colorpicker keybinding

--- config.h
+++ config.h
@@ -70,11 +70,13 @@ static const char *dtime[]  = { "/usr/bin/dtime", NULL };
 static const char *screenshotsave[]  = { "/usr/bin/screen-save", NULL };
 static const char *clipmenucmd[] = { "clipmenu", "-c", "-l", "15", "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *emojimenucmd[]  = { "/usr/bin/emojimenu", NULL };
+static const char *colorpickercmd[]  = { "/usr/bin/colorpicker", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = emojimenucmd } },
+	{ MODKEY,                       XK_x,      spawn,          {.v = colorpickercmd } },
 	{ MODKEY,                       XK_v,      spawn,          {.v = clipmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
